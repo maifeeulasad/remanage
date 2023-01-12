8f3712bf672c9ce12a631fad0e3f514e8711d188
Maifee Ul Asad <maifeeulasad@gmail.com>
2023-01-12 23:15:01 +0600

    [indexed-db]: only loading state; db state related call inside hook;

--- src/database/local/hooks/indexed-db-hooks.ts
+++ src/database/local/hooks/indexed-db-hooks.ts
@@ -2,27 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { IColumn } from '../../../component/kanban/kanban.types';
 import { getColumnDB, initDb, setColumnDB } from '../indexed-db';
 
-enum DbState {
-  LOADING = 0,
-  COMPLETED = 1,
-  BROKEN = 2,
-}
-
 const kanbanDb = () => {
-  const [dbState, setDbState] = useState(DbState.LOADING);
+  const [loading, setLoading] = useState(true);
   const [db, setDb] = useState<IDBDatabase | undefined>(undefined);
+  const [kanbanColumnsState, setKanbanColumnsState] = useState<IColumn[]>([]);
 
   useEffect(() => {
     initDb().then((idb) => {
       setDb(idb);
+      setLoading(false);
     });
   }, []);
-
-  useEffect(() => {
-    if (db !== undefined) {
-      setDbState(DbState.COMPLETED);
-    }
-  }, [db]);
 
   const getColumn = () => {
     if (db) {
@@ -31,14 +21,22 @@ const kanbanDb = () => {
     throw new Error('DB not initialized');
   };
 
-  const setColumn = (columns: IColumn[]) => {
+  useEffect(() => {
     if (db) {
-      return setColumnDB(db, columns);
+      getColumn().then((dbColumns) => setKanbanColumnsState(dbColumns));
+    }
+  }, [db, loading]);
+
+  const setKanbanColumns = (dbColumns: IColumn[]) => {
+    if (db) {
+      setColumnDB(db, dbColumns).then(() => {
+        setKanbanColumnsState(dbColumns);
+      });
     }
     throw new Error('DB not initialized');
   };
 
-  return { getColumn, setColumn, dbState };
+  return { kanbanColumns: kanbanColumnsState, setKanbanColumns, loading };
 };
 
-export { kanbanDb, DbState };
+export { kanbanDb };
