f8ba008b8b9b560a89e6c37991182d4745297a3a
Maifee Ul Asad <maifeeulasad@gmail.com>
2023-06-14 21:55:43 +0600

    [feature]: feature to copy trace id/commit hash; closes #3;

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
+import { notification } from 'antd';
 import { Footer as AntdFooter } from 'antd/lib/layout/layout';
+import CopyOutlined from '@ant-design/icons/CopyOutlined';
+// @ts-ignore
+import { copyText } from 'copy-clipboard-js';
 
 import styles from './footer.module.scss';
 
-const Footer = () => (
-  <AntdFooter className={styles.footer}>
-    <div className={styles.footerTextCenter}>
-      <div>
-        {/* @ts-ignore */}
-        Trace: {__HEAD_COMMIT_HASH__}
-      </div>
-      <div>
-        &copy; {new Date().getFullYear()} - Maifee Ul Asad
-      </div>
-    </div>
-  </AntdFooter>
-);
+// @ts-ignore
+const TRACE: string = __HEAD_COMMIT_HASH__;
+
+const Footer = () => {
+  const [api, contextHolder] = notification.useNotification();
+
+  const copyLink = () => {
+    copyText(TRACE);
+    api.open({
+      key: TRACE,
+      message: 'Trace ID copied to clipboard',
+      description: `ID: ${TRACE}`,
+      duration: 2,
+      closeIcon: <div />,
+    });
+  };
+
+  return (
+    <>
+      {contextHolder}
+      <AntdFooter className={styles.footer}>
+        <div className={styles.footerTextCenter}>
+          <div>
+            Trace: {TRACE}
+            <CopyOutlined onClick={() => copyLink()} />
+          </div>
+          <div>
+            &copy; {new Date().getFullYear()} - Maifee Ul Asad
+          </div>
+        </div>
+      </AntdFooter>
+    </>
+  );
+};
 
 export { Footer };
