abd2ac36a4a098a1cc660538a716cbd0a0a60eaf
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-05-24 12:35:43 +0600

    [fix]: fixed serve url to `:4173` over `:3000`;

--- cypress/e2e/add-item.cy.ts
+++ cypress/e2e/add-item.cy.ts
@@ -1,13 +1,13 @@
 describe('seed test', () => {
     it('tests opening modal on add item click', () => {
-        cy.visit('http://localhost:3000/remanage')
+        cy.visit('http://localhost:4173/remanage')
         cy.wait(2000)
         cy.get("[data-cy='add-item-modal']").should('not.exist')
         cy.get("[data-cy='add-item']").click()
         cy.get("[data-cy='add-item-modal']")
     })
     it('tests opened modal closing on outside click', () => {
-        cy.visit('http://localhost:3000/remanage')
+        cy.visit('http://localhost:4173/remanage')
         cy.wait(2000)
         cy.get("[data-cy='add-item-modal']").should('not.exist')
         cy.get("[data-cy='add-item']").click()
--- cypress/e2e/init.cy.ts
+++ cypress/e2e/init.cy.ts
@@ -1,6 +1,6 @@
 describe('ui test', () => {
   it('loads remanage', () => {
-    cy.visit('http://localhost:3000/remanage')
+    cy.visit('http://localhost:4173/remanage')
   })
 })
 
