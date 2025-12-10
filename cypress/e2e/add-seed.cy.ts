279ddbeb8ab994456526beb00a99240b5ea68616
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-12-10 14:22:54 +0600

    [test]: change port for seed adding;l

--- cypress/e2e/add-seed.cy.ts
+++ cypress/e2e/add-seed.cy.ts
@@ -1,6 +1,6 @@
 describe('seed test', () => {
     it('tests loading seed data on add seed click', () => {
-        cy.visit('http://localhost:3000/remanage')
+        cy.visit('http://localhost:5173/remanage')
         cy.wait(2000)
         cy.get("[data-cy='kanban-card']").should('not.exist')
         cy.get("[data-cy='add-seed']").click()
